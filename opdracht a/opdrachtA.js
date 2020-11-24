bd8588e88e45ee3341c1ce27af19d81489762f27
Jason R <99057964@mydavinci.nl>
2020-11-24 13:17:10 +0100

    Opdracht A weekdagen af.

---
+++ opdracht a/opdrachtA.js
@@ -0,0 +1,8 @@
+//Opdracht A 
+//Arrays.
+//Lab 2.
+
+var weekdays = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag'];//Alle weekdagen.
+console.log(weekdays.length);//Laat in de console zien hoeveel waardes in de array zitten.
+
+document.write("<h2>" + weekdays);
