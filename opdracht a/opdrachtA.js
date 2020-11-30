bf0707dc41e5000c56c26fa4eca21d6e60ef3fa5
Jason R <99057964@mydavinci.nl>
2020-11-30 14:59:52 +0100

    Ben opdracht A aan het herbouwen.

--- opdracht a/opdrachtA.js
+++ opdracht a/opdrachtA.js
@@ -6,22 +6,7 @@ var weekDays = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag',
 var weekLen = weekDays.length;//Een variabele met daarin hoelang de array is. In dit geval 6.
 console.log(weekLen);//Laat in de console zien hoeveel waardes in de array zitten.
 
-document.write("<h2>" + "Alle dagen van de week zijn:" + "<br>" + weekDays + "<br>");
 
-document.write("<h2>" + "De werkdagen zijn:" + "<br>");
-document.write(weekDays[0], weekDays[1],weekDays[2],weekDays[3],weekDays[4] + "<br>" + "<br>");
-
-document.write("De weekenddagen zijn:");
-document.write(weekDays[5],weekDays[6]+ "<br>" + "<br>");
-
-document.write("Alle dagen van de week in omgekeerde volgorde zijn:" + "<br>");
-document.write(weekDays[6],weekDays[5],weekDays[4],weekDays[3],weekDays[2],weekDays[1],weekDays[0] + "<br>" + "<br>");
-
-document.write("De werkdagen in omgekeerde volgorde zijn:");
-document.write(weekDays[4], weekDays[3],weekDays[2],weekDays[1],weekDays[0] + "<br>" + "<br>");
-
-document.write("De weekenddagen in omgekeerde volgorde zijn:" + "<br>");
-document.write(weekDays[6],weekDays[5]);
 
 
 
