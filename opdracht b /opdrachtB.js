c977f2cb6192d97aca55f31fac8f6a3129be38a1
Jason R <99057964@mydavinci.nl>
2020-12-01 12:32:13 +0100

    Heb de eerste 3 opdrachten af van opdracht B.
    Heb de optrek functie,de aftrek functie, de vermenigvuldig functie af.

--- opdracht b /opdrachtB.js
+++ opdracht b /opdrachtB.js
@@ -1,8 +1,35 @@
 //Opdracht B.
+//Lab 2.
 
-var arrayEen = [1,2,3,4,5,6,7,8,9,10];
-var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
+var arrayEen = [1,2,3,4,5,6,7,8,9,10];//Eerste Array.
+var arrayTwee = [2,4,6,8,10,12,14,16,18,20];//Tweede Array.
 
-function optellen(arrayEen,arrayTwee){
-    
+function optellen(){//Functie voor de optel opdrachr.
+    for(i=0; i <=9; i++){
+        var antwoord = arrayEen[i] + arrayTwee[i];//Variabale van de antwoorden van de 2 arrays met daarin de indexen dat bij elkaar geteld word.
+        document.write(arrayEen[i] + '+' + arrayTwee[i] + '=' + antwoord + "<br>");//Uitwerking van de opdracht.
+    }
+    document.write("<br>");
 }
+
+optellen();//Om de functie aan te roepen.
+
+function aftrekken(){
+    for(i=0; i <=9; i++){
+        var antwoord = arrayTwee[i] - arrayEen[i];//
+        document.write(arrayEen[i] + '-' + arrayTwee[i] + '=' + antwoord + "<br>");//Uitwerking van de opdracht.
+    }
+    document.write("<br>");
+}
+
+aftrekken();//Om de functie aan te roepen.
+
+function vermenigvuldigen(){
+    for(i=0; i <=9; i++){
+        var antwoord = arrayTwee[i] * arrayEen[i];//
+        document.write(arrayEen[i] + '*' + arrayTwee[i] + '=' + antwoord + "<br>");//Uitwerking van de opdracht.
+    }
+    document.write("<br>");
+}
+
+vermenigvuldigen();//Om de functie aan te roepen.
